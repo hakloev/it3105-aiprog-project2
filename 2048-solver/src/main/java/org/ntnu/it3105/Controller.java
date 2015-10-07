25e21e8bb1c866f4610e71f35bd5af7b0b57ba28
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-10-07 20:29:59 +0200

    not working now, but added available cells list

--- 2048-solver/src/main/java/org/ntnu/it3105/Controller.java
+++ 2048-solver/src/main/java/org/ntnu/it3105/Controller.java
@@ -5,6 +5,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import org.apache.log4j.Logger;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Håkon Ødegård Løvdal (hakloev) on 07/10/15.
  * <p>
@@ -50,10 +54,24 @@ public class Controller {
         scoreLabel.setText(String.valueOf(currentScore));
 
         addTile();
-        //addTile();
+        addTile();
     }
 
-    private int curr_x = 1, curr_y = 2;
+    /**
+     * Returns a list of all possible positions to add a tile
+     */
+    private List<Point> getAllFreeCells() {
+        List<Point> freeCells = new ArrayList<>();
+        for (int y = 0; y < BOARD_HEIGHT; y++) {
+            for (int x = 0; x < BOARD_WIDTH; x++) {
+                if (tiles[y][x] == null) {
+                    freeCells.add(new Point(x, y));
+                }
+            }
+        }
+        // TODO: If list empty, game is lost
+        return freeCells;
+    }
 
     /**
      * Add a single tile to the board
@@ -61,11 +79,14 @@ public class Controller {
      * The tile should be 2 or 4. Ensure a probability of 0.9 for 2 and 0.1 for 4
      */
     private void addTile() {
-        // TODO: GET LIST OF POSSIBLE POSITIONS
-        // TODO: ADD TILE TO POSITION
+        List<Point> allFreeCells = getAllFreeCells();
+        int cellToPopulate = (int)(Math.random() * allFreeCells.size());
+        Point cord = allFreeCells.get(cellToPopulate);
 
-        tiles[curr_x][curr_y] = Math.random() < 0.9 ? new Tile(2) : new Tile(4);
-        grid.add(tiles[curr_x][curr_y], curr_x, curr_y);
+        log.debug("Adding tile to: (" + cord.x + ", " + cord.y + ")");
+
+        tiles[cord.x][cord.y] = Math.random() < 0.9 ? new Tile(2) : new Tile(4);
+        grid.add(tiles[cord.x][cord.y], cord.x, cord.y);
     }
 
 
@@ -73,8 +94,10 @@ public class Controller {
      * Moves all tiles and promotes (to the power of 2) them if possible
      * @param direction
      */
-    public void doMove(Direction direction) {
+    public boolean doMove(Direction direction) {
         log.info("Moving in direction: " + direction);
+
+        /*
         // TODO: MOVE ALL TILES, NOT JUST THIS HARDCODED ONE
         int from_x = curr_x, from_y = curr_y;
         int to_x = (from_x + direction.x), to_y = (from_y + direction.y);
@@ -86,5 +109,7 @@ public class Controller {
 
         curr_x = to_x;
         curr_y = to_y;
+        */
+
     }
 }
