e9e6edbca63d931f5275c262b71a384d54d4eaf3
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-10-12 20:42:38 +0200

    pseudocode

--- 2048-solver/src/main/java/org/ntnu/it3105/ai/Expectimax.java
+++ 2048-solver/src/main/java/org/ntnu/it3105/ai/Expectimax.java
@@ -24,6 +24,28 @@ public class Expectimax {
         this.depthLimit = depthLimit;
     }
 
+    /*
+    function expectiminimax(node, depth)
+    if node is a terminal node or depth = 0
+        return the heuristic value of node
+    if the adversary is to play at node
+        // Return value of minimum-valued child node
+        let α := +∞
+        foreach child of node
+            α := min(α, expectiminimax(child, depth-1))
+    else if we are to play at node
+        // Return value of maximum-valued child node
+        let α := -∞
+        foreach child of node
+            α := max(α, expectiminimax(child, depth-1))
+    else if random event at node
+        // Return weighted average of all child nodes' values
+        let α := 0
+        foreach child of node
+            α := α + (Probability[child] * expectiminimax(child, depth-1))
+    return α
+     */
+
     public Direction getNextMove() {
         Direction nextMove = Direction.LEFT;
         return nextMove;
